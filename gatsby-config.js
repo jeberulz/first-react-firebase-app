8d1994ea158b1fa1410a90cd497eb827a5237a44
John Iseghohi <john.iseghohi@booking.com>
2018-08-18 12:31:22 +0200

    quick update

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'My First FireBase Project',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>My first firesbase + react app</h1>
+    <p>Let's explore the best way to use this tools</p>
+    
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
