fc848de1c22f9170dfa709051c02cd655da973fc
John Iseghohi <john.iseghohi@booking.com>
2018-08-18 16:48:49 +0200

    Updated design - CSS grid

--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,21 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import './Header.css'
+
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <div className="Header">
+    <div className="HeaderGroup">
+    <Link to="/"> Logo here </Link>
+    <Link to="/">About me</Link>
+    <Link to="/">Contact</Link>
+    <Link to="/">Profile</Link>
+
+    <Link to="/"><button>Buy</button></Link>
+    
+    
     </div>
+
   </div>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,16 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const IndexPage = () => (
-  <div>
-    <h1>My first firesbase + react app</h1>
-    <p>Let's explore the best way to use this tools</p>
-    
-    <Link to="/page-2/">Go to page 2</Link>
+<div>
+<header/>
+  <div className="Hero">
+    <div className="HeroGroup">
+      <h1>My first firesbase + react app</h1>
+      <p>Let's explore the best way to use this tools</p>
+      <Link to="/page-2/">WATCH THE NOW</Link>
+    </div>
   </div>
+</div>
 )
 
 export default IndexPage
